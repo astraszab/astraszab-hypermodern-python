4a0d6874faa201b1abc6e094c0446fbebf091926
astraszab <u.astraszab@gmail.com>
2021-10-14 12:10:42 +0300

    Reformat console.py with black

--- src/astraszab_hypermodern_python/console.py
+++ src/astraszab_hypermodern_python/console.py
@@ -15,7 +15,7 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
     default="en",
     help="Language edition of Wikipedia",
     metavar="LANG",
-    show_default=True
+    show_default=True,
 )
 @click.version_option(version=__version__)
 def main(language):
