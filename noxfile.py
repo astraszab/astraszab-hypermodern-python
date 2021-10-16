b70e2514c08bec4d7a231160c06ea917910b2d7a
astraszab <u.astraszab@gmail.com>
2021-10-16 22:14:57 +0300

    Add type stubs for click

--- noxfile.py
+++ noxfile.py
@@ -38,7 +38,7 @@ def lint(session):
 @nox.session(python=["3.9", "3.8"])
 def mypy(session):
     args = session.posargs or locations
-    install_with_constraints(session, "mypy", "types-requests")
+    install_with_constraints(session, "mypy", "types-click", "types-requests")
     session.run("mypy", *args)
 
 
