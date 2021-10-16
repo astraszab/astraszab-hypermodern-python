a280987907c51c71bfe3359c15ff60899163a7bb
astraszab <u.astraszab@gmail.com>
2021-10-16 22:56:58 +0300

    Add type annotations to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,18 @@
+from unittest.mock import Mock
+
+from _pytest.config import Config
 import pytest
+from pytest_mock import MockFixture
 
 from astraszab_hypermodern_python import wikipedia
 
 
-def pytest_configure(config):
+def pytest_configure(config: Config) -> None:
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
+def mock_requests_get(mocker: MockFixture) -> Mock:
     mock = mocker.patch("requests.get")
     mock.return_value.__enter__.return_value.json.return_value = {
         "title": "Lorem Ipsum",
@@ -18,7 +22,7 @@ def mock_requests_get(mocker):
 
 
 @pytest.fixture
-def mock_wikipedia_random_page(mocker):
+def mock_wikipedia_random_page(mocker: MockFixture) -> Mock:
     mock = mocker.patch("astraszab_hypermodern_python.wikipedia.random_page")
     mock.return_value = wikipedia.Page(
         title="Lorem Ipsum", extract="Lorem ipsum dolor sit amet"
