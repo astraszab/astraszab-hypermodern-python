d2147008d59bd2104fd8643cb91899aa52424fdf
astraszab <u.astraszab@gmail.com>
2021-10-16 22:36:34 +0300

    Annotate noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,15 @@
 import tempfile
+from typing import Any
 
 import nox
+from nox.sessions import Session
 
 
 nox.options.sessions = "lint", "mypy", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
-def install_with_constraints(session, *args, **kwargs):
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -22,7 +24,7 @@ def install_with_constraints(session, *args, **kwargs):
 
 
 @nox.session(python=["3.9", "3.8"])
-def lint(session):
+def lint(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(
         session,
@@ -36,14 +38,14 @@ def lint(session):
 
 
 @nox.session(python=["3.9", "3.8"])
-def mypy(session):
+def mypy(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "mypy", "types-click", "types-requests")
     session.run("mypy", *args)
 
 
 @nox.session(python="3.9")
-def safety(session):
+def safety(session: Session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -59,7 +61,7 @@ def safety(session):
 
 
 @nox.session(python=["3.9", "3.8"])
-def tests(session):
+def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
@@ -69,7 +71,7 @@ def tests(session):
 
 
 @nox.session(python=["3.9"])
-def black(session):
+def black(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "black")
     session.run("black", *args)
